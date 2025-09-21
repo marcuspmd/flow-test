7b8a0dc91f5845b1f37f61e5f6c4218e04459122
Marcus Paulo M Dias <marcusmazzon@gmail.com>
2025-09-20 22:24:24 -0300

    feat: adicionar configuração do adaptador para o Astro e atualizar dependências do projeto

--- report-dashboard/astro.config.mjs
+++ report-dashboard/astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 export default defineConfig({
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
